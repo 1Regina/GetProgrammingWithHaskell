0ed198db36ec62bbdcc143de7e5fdfe6e1533aac
1Regina <46968488+1Regina@users.noreply.github.com>
2020-10-12 23:29:15 +0800

    fix problems on file with type signature

--- unit1/lesson5/genFIfEven.hs
+++ unit1/lesson5/genFIfEven.hs
@@ -1,5 +1,7 @@
+ifEven :: Integral p => (p -> p) -> p -> p
 ifEven f x = if even x
              then f x
              else x
 
+genIfXEven :: Integral p => p -> (p -> p) -> p
 genIfXEven x = (\f -> ifEven f x)
