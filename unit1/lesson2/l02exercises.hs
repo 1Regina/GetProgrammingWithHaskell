d49111dc2292cfbb75e5543aa5f2309ad239c45f
1Regina <46968488+1Regina@users.noreply.github.com>
2021-06-14 18:58:42 +0800

    Attempt 2.1

--- unit1/lesson2/l02exercises.hs
+++ unit1/lesson2/l02exercises.hs
@@ -1,3 +1,6 @@
+--Q2.1
+-- Bcos every argument must return a value so the else statement must be included to handle values for alternative arguments.
+
 --Q2.2
 inc :: Num a => a -> a
 inc x = x + 1
@@ -16,7 +19,7 @@ square x = x^2
 check :: Integral a => a -> a
 check n = if n `mod` 2 == 0
             then n - 2
-          else 3 * n + 1  
+          else 3 * n + 1
 
 -- >>> check 4
 -- 2
@@ -25,7 +28,7 @@ check n = if n `mod` 2 == 0
 check1 :: Integral a => a -> a
 check1 n = if even n
            then n - 2
-          else 3 * n + 1 
+          else 3 * n + 1
 -- >>> check1 4
 -- 2
 --
