38d37b90ae82db952cc5a375d3c12133eeaa9322
1Regina <46968488+1Regina@users.noreply.github.com>
2021-06-14 18:45:52 +0800

    Notes from Unit 1

---
+++ README.md
@@ -0,0 +1,5 @@
+Unit 1
+1. 3 rules on haskell functions
+   1. All functions must take an argument
+   2. All functions must return a value
+   3. Anytime a function is called with the same argument, it must return the same value. Thus python += cannot exist in Haskell
--- unit1/lesson2/calChange.hs
+++ unit1/lesson2/calChange.hs
@@ -1,6 +1,14 @@
 
 calChange :: (Ord p, Num p) => p -> p -> p
-calChange owed given = if change > 0 
+calChange owed given = if change > 0
                            then change
-                        else 0  
+                        else 0
                  where change = given - owed
+
+
+doublePlusTwo :: Num a => a -> a
+doublePlusTwo x = doubleX  + 2
+   where doubleX = 2 * x
+
+-- >>> doublePlusTwo 2
+-- 6
