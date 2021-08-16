2e631c0c25e66c1fcb2aa85006bd58c7e8e06e29
1Regina <46968488+1Regina@users.noreply.github.com>
2021-08-16 22:01:57 +0800

    question getContents

--- unit4/lesson22/l22_1exercises.hs
+++ unit4/lesson22/l22_1exercises.hs
@@ -1,5 +1,6 @@
 import System.Environment
 import Control.Monad
+-- import Data.List.Split (splitOn) -- NA for without cabal
 
 
 -- -- Q22.1    Write a program, simple_calc.hs, that reads simple equations involving adding two numbers or multiplying two numbers. The program should solve the equation each user types into each line as each line is entered.
@@ -39,14 +40,83 @@ import Control.Monad
 main :: IO ()
 main = do
     putStrLn "Is this addition or multiplication"
-    answer <- getContents
-    if read answer == "addition"
-
-          then do
+    answer <- getLine
+    -- if answer == "addition"
+    --       then do
+    --           putStrLn "write your addition equation"
+    --           calSum <- getLine
+    --           return ()
+    --       else do
+    --           putStrLn "write your multiplication equation"
+    --           calProduct <- getLine
+    --           let p = read calProduct :: String
+    --           return ()
+    case answer of
+        "addition" -> do
               putStrLn "write your addition equation"
-              calSum <- getContents
-              read calSum
-          else do
-              putStrLn "write your multiplication equation"
-              calProduct <- getContents
-              read calProduct
+              calSum <- getLine
+              let [a,b] = split '+' calSum
+              -- alt: use case of instead
+              -- let ab = split '+' calSum
+              -- case ab of [a,b] -> ...; _ -> error
+              print $ read a + read b
+        "multiplication" -> do
+              putStrLn "write your multiplicat equation"
+              calProd <- getLine
+              let [a,b] = split '*' calProd
+              -- return ()
+              print $ read a * read b
+        _ -> do
+            putStrLn "pls select again"
+            main
+
+-- ddd+ddd
+
+
+split :: Eq a => a -> [a] -> [[a]]
+split needle [] = []
+split needle (a : as)
+   | needle == a = [] : split needle as
+   | otherwise = case split needle as of
+        [] -> [[a]]
+        as' : ass -> (a: as') : ass
+
+
+
+-- >>> split '+' "+68"
+-- ["","68"]
+
+-- >>> split '+' "+34+56"
+-- "" : ["34","56"]
+
+-- >>> split '+' "4+68"
+-- ["4","68"]
+
+-- >>> split '+' "34+68"
+-- ["34","68"]
+
+
+-- ["34","68"]
+-- (a: as') : ass
+
+
+-- needle = '+'
+-- a = '3'
+-- >>> split '+' "4+68"
+-- ["4","68"]
+
+-- "4":"68":[]
+-- as' = "4"
+-- ass = "68":[]
+
+
+-- >>> split '+' ""
+-- []
+
+
+-- >>> split '+' "8"
+-- ["8"]
+-- [['8']]
+
+-- >>> split '+' "68"
+-- ["68"]
